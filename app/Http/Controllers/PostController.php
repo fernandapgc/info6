b02815fca60be66374bb8647002ee7b71aa9abcf
fernandapgc <fernanda.pgc2@gmail.com>
2022-04-07 21:23:27 -0500

    Clase 8 cambios delete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -111,8 +111,11 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Posts $post)
     {
-        //
+        //dd($post); 
+        $post->delete(); 
+        return back()->with('status', 'Post deleted successfully');
+        
     }
 }
